0a6c6e5ad78c15b1ef30e9d83fcbb5f2cb33bda9
BrownBearDude <48665034+BrownBearDude@users.noreply.github.com>
2020-10-01 12:25:01 +1000

    cleaned code a little
    cleaned code and replaced temp var names.

--- pathdfindintake2/test_1.gd
+++ pathdfindintake2/test_1.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 #storage of shapes with neirbours, might be a bad way to store shapes, 
 #I don'know and don't care, might look into it in the future. 
 var nodes = {
@@ -24,71 +23,11 @@ var nodes = {
 var keys = nodes.keys() 
 var points_at_point = []
 
-
 func _ready():
-	_points_at_slice(nodes)
-
-
-
-func _points_at_slice(nodes):
 	#drawing stuff
 	set_process(true)
-	
-	
-	keys.sort_custom(self, "custom_sort")
-	print_debug(keys)
-	
-	#will rename variables in the furture 
-	var num = 0
-	var temp = [] #the current slice
-	var temp_2 = [] # an array of current slices
-	
-	while num < keys.size():
-		var index = null
-		temp_2 = temp_2.duplicate(true)
-		
-		if nodes[keys[num]][0].x >= keys[num].x and nodes[keys[num]][1].x >= keys[num].x:
-			index = (_increase_in_conn(temp, keys, nodes, num))
-			if index == null: index = 0
-			temp.insert(index, keys[num])
-			temp.insert(index, keys[num])
-			temp_2.append(temp)
-#			print(temp_2[temp_2.size()-1])
-		
-		
-		
-		# this just doesn't work it outputs a blank array, why, why
-		elif nodes[keys[num]][0].x <= keys[num].x and nodes[keys[num]][1].x <= keys[num].x:
-			var temp_3 = temp.duplicate(true)
-#			print("decrease")
-			
-			index = _decrease_in_conn(temp, keys, nodes, num, 0)
-			if index != null:
-				temp[index] = keys[num]
-				temp_3.remove(index)
-			
-			index = _decrease_in_conn(temp, keys, nodes, num, 1)
-			if index != null:
-				temp[index] = keys[num]
-				temp_3.remove(index)
-			
-			temp_2.append(temp)
-			temp = temp_3.duplicate(true)
-#			print(temp_2[temp_2.size()-1])
-		
-		else:
-			index = _not_increase_in_conn(temp, keys, nodes, num)
-			if index != null:
-				temp[index] = keys[num]
-				temp_2.append(temp)
-#				print(temp_2[temp_2.size()-1])
-		num += 1
-	
-	
-	for ob in temp_2:
+	for ob in _points_at_slice(nodes):
 		print(ob)
-	
-
 
 #needed for drawing 
 func _process(delta): 
@@ -98,15 +37,58 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		print(get_global_mouse_position())
 
-
 #draw the outline of the big shape. 
 func _draw():
 	for ob in nodes:
-		var temp_2 = ob
-		var temp = nodes[ob]
-		for ob in temp:
-			draw_line(ob, temp_2, Color.white, 10.0)
+		var slice_array = ob
+		var slice = nodes[ob]
+		for ob in slice:
+			draw_line(ob, slice_array, Color.white, 10.00)
 
+
+func _points_at_slice(nodes):
+
+	keys.sort_custom(self, "custom_sort")
+	print_debug(keys)
+	
+	#will rename variables in the furture 
+	var num = 0
+	var slice = [] #the current slice
+	var slice_array = [] # an array of current slices
+	
+	while num < keys.size():
+		var index = null
+		slice_array = slice_array.duplicate(true)
+		if nodes[keys[num]][0].x >= keys[num].x and nodes[keys[num]][1].x >= keys[num].x:
+			index = (_increase_in_conn(slice, keys, nodes, num))
+			if index == null: index = 0
+			slice.insert(index, keys[num])
+			slice.insert(index, keys[num])
+			slice_array.append(slice)
+#			print(slice_array[slice_array.size()-1])
+		elif nodes[keys[num]][0].x <= keys[num].x and nodes[keys[num]][1].x <= keys[num].x:
+			var slice_save_point = slice.duplicate(true)
+#			print("decrease")
+			index = _decrease_in_conn(slice, keys, nodes, num, 0)
+			if index != null:
+				slice[index] = keys[num]
+				slice_save_point.remove(index)
+			index = _decrease_in_conn(slice, keys, nodes, num, 1)
+			if index != null:
+				slice[index] = keys[num]
+				slice_save_point.remove(index)
+			slice_array.append(slice)
+			slice = slice_save_point.duplicate(true)
+#			print(slice_array[slice_array.size()-1])
+		else:
+			index = _not_increase_in_conn(slice, keys, nodes, num)
+			if index != null:
+				slice[index] = keys[num]
+				slice_array.append(slice)
+#				print(slice_array[slice_array.size()-1])
+		num += 1
+	
+	return slice_array
 
 
 #sort based on x value if there the same y value 
@@ -119,29 +101,25 @@ func custom_sort(a, b):
 	return false 
 
 
-#find inbetween what it is and shove it in there. 
-func _increase_in_conn(temp, keys, nodes, num):
-	if !(temp.empty()):
+func _increase_in_conn(slice, keys, nodes, num):
+	if !(slice.empty()):
 		var i = 0
-		for ob in temp:
+		for ob in slice:
 			if keys[num].y <= ob.y: 
 				return i
 			i += 1
-		return temp.size()
+		return slice.size()
 	else:
 		return 0
 
-
-func _decrease_in_conn(temp, keys, nodes, num, fuck):
-	var index = temp.find(nodes[keys[num]][fuck])
+func _decrease_in_conn(slice, keys, nodes, num, not_a_swear):
+	var index = slice.find(nodes[keys[num]][not_a_swear])
 	if index >= 0 : return index
-
-
-#find brother and replace it 
-func _not_increase_in_conn(temp, keys, nodes, num):
-	var index = temp.find(nodes[keys[num]][0])
+ 
+func _not_increase_in_conn(slice, keys, nodes, num):
+	var index = slice.find(nodes[keys[num]][0])
 	if index >= 0 : return index
 	else:
-		index = temp.find(nodes[keys[num]][1])
+		index = slice.find(nodes[keys[num]][1])
 		if index >= 0: return index 
 		else: return  
